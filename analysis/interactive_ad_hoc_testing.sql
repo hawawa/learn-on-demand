aca38dd0da55e5cae4df65c517decc7e8bd0f8e6
Chih-Hsi Chang <changchihhsi@gmail.com>
2026-08-12 07:51:22 +0000

    practice 1

---
+++ analysis/interactive_ad_hoc_testing.sql
@@ -0,0 +1,1 @@
+select customer_id from {{ ref('stg_customers') }} group by customer_id having count(*) > 1
--- models/staging/snowplow/stg_page_views.sql
+++ models/staging/snowplow/stg_page_views.sql
@@ -2,16 +2,22 @@
     materialized = 'incremental',
     unique_key = 'page_view_id'
 ) }}
+
 with events as (
-    select * from {{ source('snowplow', 'events') }}
+    select
+        *,
+        event_id as page_view_id
+    from {{ source('snowplow', 'events') }}
     {% if is_incremental() %}
     where collector_tstamp >= (select max(max_collector_tstamp) from {{ this }})
     {% endif %}
 ),
+
 page_views as (
     select * from events
     where event = 'page_view'
 ),
+
 aggregated_page_events as (
     select
         page_view_id,
@@ -21,10 +27,15 @@ aggregated_page_events as (
     from events
     group by 1
 ),
+
 joined as (
     select
-        *
+        page_views.*,
+        aggregated_page_events.approx_time_on_page,
+        aggregated_page_events.page_view_start,
+        aggregated_page_events.max_collector_tstamp
     from page_views
     left join aggregated_page_events using (page_view_id)
 )
+
 select * from joined
